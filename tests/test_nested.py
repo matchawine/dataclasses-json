d9e3e0eba192d9a064f59221d23924012cb448ce
eqperes <eqperes@gmail.com>
2020-01-13 14:17:48 -0300

    check if nested dataclasses errors are raised the same as non-nested

--- tests/test_nested.py
+++ tests/test_nested.py
@@ -23,3 +23,20 @@ class TestDecoder:
     def test_nested_list_of_dataclasses(self):
         assert (DataClassXs.from_json('{"xs": [{"x": 0}, {"x": 1}]}') ==
                 DataClassXs([DataClassX(0), DataClassX(1)]))
+
+    def test_wrong_nested_list_of_dataclasses(self):
+        try:
+            _ = DataClassX.from_json('{"a": "incorrect_field"}')
+            x_error = None
+        except Exception as e:
+            x_error = e
+        assert x_error is not None
+
+        try:
+            _ = DataClassXs.from_json('{"xs": [{"a": "incorrect_field"}]}')
+            xs_error = None
+        except Exception as e:
+            xs_error = e
+        assert xs_error is not None
+        assert x_error.__class__ == xs_error.__class__
+        assert x_error.args == xs_error.args
